432012d2f0d8f2a000d56099cb7cb09871cb0e5a
juyounglee08 <coderjuyoung82@gmail.com>
2025-07-08 09:47:12 +0900

    마지막  커밋

--- app/core/config.py
+++ app/core/config.py
@@ -3,10 +3,10 @@ from typing import Optional
 
 class Settings(BaseSettings):
     # Database
-    DATABASE_URL: str = "mysql+pymysql://pharmacy_user:pharmacy_pass@mysql:3306/pharmacy_db"
+    DATABASE_URL: str = "mysql+pymysql://pharmacy_user:root@1234:3306/pharmacy_db"
     
     # Security
-    SECRET_KEY: str = "your-secret-key-change-in-production"
+    SECRET_KEY: str = "FH3fivjwNsdp8hVWBT7wIF/2Hxi7gQIA7bu19DxYK6Ld111bKFoCRHDw2AL5nq9Oa48+2pa+ktvsGWO3kcjupQ=="
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     
